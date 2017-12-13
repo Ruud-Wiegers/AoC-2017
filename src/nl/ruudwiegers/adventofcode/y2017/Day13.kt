2ffb892a0b1efb42affe9e34c7e5818399fa1242
Ruud Wiegers <ruud.wiegers@topicus.nl>
2017-12-13 08:47:45 +0100

    Day 13. Another nicely succinct one. Legible, too, except for the important part.

--- src/nl/ruudwiegers/adventofcode/y2017/Day13.kt
+++ src/nl/ruudwiegers/adventofcode/y2017/Day13.kt
@@ -1,2 +1,27 @@
 package nl.ruudwiegers.adventofcode.y2017
 
+import nl.ruudwiegers.adventofcode.AdventSolution
+
+object Day13 : AdventSolution(2017, 13) {
+
+    override fun solvePartOne(input: String): String = parseInput(input)
+            .filter { (depth, range) -> depth % (range * 2 - 2) == 0 }
+            .entries
+            .sumBy { (depth, range) -> depth * range }
+            .toString()
+
+    override fun solvePartTwo(input: String): String {
+        val lasers = parseInput(input)
+        val shortestSafeDelay = generateSequence(0) { it + 1 }
+                .indexOfFirst { delay ->
+                    lasers.none { (depth, range) ->
+                        (depth + delay) % (range * 2 - 2) == 0
+                    }
+                }
+        return shortestSafeDelay.toString()
+    }
+
+    private fun parseInput(input: String) = input.split("\n")
+            .map { it.split(": ") }
+            .associate { (depth, range) -> depth.toInt() to range.toInt() }
+}
