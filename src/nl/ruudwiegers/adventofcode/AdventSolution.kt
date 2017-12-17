71a20752490266a2c7e9df6e286239d59c265add
Ruud Wiegers <ruud.wiegers@topicus.nl>
2017-12-17 11:15:40 +0100

    caching

--- src/nl/ruudwiegers/adventofcode/AdventSolution.kt
+++ src/nl/ruudwiegers/adventofcode/AdventSolution.kt
@@ -1,8 +1,14 @@
 package nl.ruudwiegers.adventofcode
 
+import java.io.File
 import java.net.URL
 
 abstract class AdventSolution(private val year: Int, private val day: Int) {
+
+    init {
+        require(year in 2015..2100) { "$year is not a valid year. AoC started in 2015" }
+        require(day in 1..25) { "$day is not a valid day in december. Choose a day in 1-25" }
+    }
 
     fun solve() {
         val input = retrieveInput()
@@ -13,15 +19,29 @@ abstract class AdventSolution(private val year: Int, private val day: Int) {
         println()
     }
 
-    private fun retrieveInput(): String {
-        require(year in 2015..2100) { "$year is not a valid year. AoC started in 2015" }
-        require(day in 1..25) { "$day is not a valid day in december. Choose a day in 1-25" }
+    private fun retrieveInput() = readFromCache() ?: readfromUrl().also { write(it) }
+
+
+    private fun readfromUrl(): String {
         return URL("http://adventofcode.com/$year/day/$day/input")
                 .openConnection()
                 .apply { addRequestProperty("Cookie", "session=" + AOC_SESSION) }
                 .getInputStream().use { it.readBytes() }
                 .toString(Charsets.UTF_8)
                 .trimEnd()
+    }
+
+    private fun write(input: String) {
+        File("aoc-$year-$day-input.txt").printWriter().use { out ->
+            out.print(input)
+        }
+    }
+
+    private fun readFromCache(): String? {
+        val file = File("aoc-$year-$day-input.txt")
+        return if (file.exists()) {
+            file.readText()
+        } else null
     }
 
     abstract fun solvePartOne(input: String): String
