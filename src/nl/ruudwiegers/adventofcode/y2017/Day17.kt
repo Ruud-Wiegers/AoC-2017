b410c10d47feaf98ed55e6810b649642b898bd6e
Ruud Wiegers <ruud.wiegers@topicus.nl>
2017-12-17 09:08:43 +0100

    Day 17.

---
+++ src/nl/ruudwiegers/adventofcode/y2017/Day17.kt
@@ -0,0 +1,28 @@
+package nl.ruudwiegers.adventofcode.y2017
+
+import nl.ruudwiegers.adventofcode.AdventSolution
+
+object Day17 : AdventSolution(2017, 17) {
+
+    override fun solvePartOne(input: String): String {
+        val step = input.toInt()
+        val spinBuffer = mutableListOf(0)
+        var pos = 0
+        for (i in 1..2017) {
+            pos = (pos + step + 1) % i
+            spinBuffer.add(pos, i)
+        }
+        return spinBuffer[pos + 1].toString()
+    }
+
+    override fun solvePartTwo(input: String): String {
+        val step = input.toInt()
+        var firstItem = 0
+        var pos = 0
+        for (i in 1..50_000_000) {
+            pos = (pos + step + 1) % i
+            if (pos == 0) firstItem = i
+        }
+        return firstItem.toString()
+    }
+}
