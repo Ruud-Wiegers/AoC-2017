a968bf4dc7d2eb82433e4905237067c215acea66
Ruud Wiegers <ruud.wiegers@topicus.nl>
2017-12-17 07:34:17 +0100

    Day 16 minor improvements.

--- src/nl/ruudwiegers/adventofcode/y2017/Day16.kt
+++ src/nl/ruudwiegers/adventofcode/y2017/Day16.kt
@@ -7,17 +7,10 @@ object Day16 : AdventSolution(2017, 16) {
     override fun solvePartOne(input: String): String = dance(parseInput(input), "abcdefghijklmnop")
 
     override fun solvePartTwo(input: String): String {
-        val moves: List<Action> = parseInput(input)
-
-        val orderings = mutableListOf("abcdefghijklmnop")
-        var newOrdering = dance(moves, orderings.last())
-        while (newOrdering !in orderings) {
-            orderings += newOrdering
-            newOrdering = dance(moves, orderings.last())
-        }
-        val startOfLoop = orderings.indexOf(newOrdering)
-        val loopSize = orderings.size - startOfLoop
-
+        val danceMoves: List<Action> = parseInput(input)
+        val orderings = applyUntilFirstRepetition("abcdefghijklmnop") { dance(danceMoves, it) }
+        val startOfLoop = orderings.indexOf(orderings.last())
+        val loopSize = orderings.size - startOfLoop - 1
         val remainder = (1_000_000_000 - startOfLoop) % loopSize
 
         return orderings[remainder]
@@ -32,6 +25,20 @@ object Day16 : AdventSolution(2017, 16) {
                     else -> throw IllegalArgumentException("$move is not a dance move")
                 }
             }
+
+
+    /* repeatedly apply the operation, until an existing result is found
+    Returns the list of intermediate values, including the final repeated value
+    The distinct-check is pretty slow, but faster versions are less legible
+    */
+    private inline fun <T> applyUntilFirstRepetition(initial: T, operation: (T) -> T): List<T> {
+        val accumulator = mutableListOf(initial)
+        do {
+            val newOrdering = operation(accumulator.last())
+            accumulator += newOrdering
+        } while (accumulator.indexOf(newOrdering) == accumulator.lastIndex)
+        return accumulator
+    }
 
     private fun dance(moves: List<Action>, initialPositions: String) = moves.fold(initialPositions) { acc, action -> action.execute(acc) }
 }
